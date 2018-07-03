226aada16af511fce8ba5a394f459747976cb4c8
xlabsvirtusa1 <xlabsvirtusa1@gmail.com>
2018-07-03 17:39:01 +0530

    Promise & Resolve implemented: sample

--- async-demo/index.js
+++ async-demo/index.js
@@ -2,8 +2,10 @@
 // Asynchronous
 console.log('Before..');
 
-const p = getuser(1);
-p.then(user => console.log(user));
+getuser(1).then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits:', commits))
+    .catch(err => console.log('Error:', err.message));
 
 console.log('After');
 
@@ -12,6 +14,23 @@ function getuser(id) {
         setTimeout(() => {
             console.log('Reading a user from database..');
             resolve({ id: id, gitHubUsername: 'mosh' });
+        }, 2000);
+    });
+}
+
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling Github API..');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['commit']);
         }, 2000);
     });
 }
