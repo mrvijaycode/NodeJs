7b2de7d40a5e64c616d00592b3a2213f1ace4b6d
xlabsvirtusa1 <xlabsvirtusa1@gmail.com>
2018-07-03 17:08:33 +0530

    implemented promise and executed

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,42 +1,17 @@
 
 // Asynchronous
 console.log('Before..');
-getuser(1, getRepositories);
 
-console.log('After..');
+const p = getuser(1);
+p.then(user => console.log(user));
 
-//The above code if in Synchronous
-/*console.log('Before');
-const user = getuser(1);
-const repos = getRepositories(user.gitHubUsername);
-const commits = getCommits(repos[0]);
 console.log('After');
-*/
 
-function getRepositories(user) {
-    getRepositories(user.gitHubUsername, getCommits);
-}
-
-
-function getCommits(repos) {
-    getCommits(repo, displayCommits);
-}
-
-
-function displayCommits(commits) {
-    console.log(commits);
-}
-
-function getuser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from database..');
-        callback({ id: id, gitHubUsername: 'mosh' });
-    }, 2000);
-}
-
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Calling Github API...');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getuser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from database..');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+        }, 2000);
+    });
 }
